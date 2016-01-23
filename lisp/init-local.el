6dda80f7440f3292aa71905136531970dc885c84
Glebov Boris <avatar29A@gmail.com>
2016-01-23 15:16:09 +0500

    Add themes to init-local.el

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,7 +1,11 @@
-;;; Commentary:
 ;;; File should contains user's custom code
 (require 'init-keybindings)
 (require 'init-fun) ;; Just for fun. You may delete it.
+
+;;; Set theme (you may uncomment required theme):
+;; (light)
+(dark)
+;;(zenburn)
 
 ;;; Code:
 
